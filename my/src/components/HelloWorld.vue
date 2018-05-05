76826e49faa20386f8ad7b63c69c3ccd3a80bcbf
Mr. sea tree <30616828+ZZXcostar@users.noreply.github.com>
2018-05-05 20:01:40 +0800

    23

--- my/src/components/HelloWorld.vue
+++ my/src/components/HelloWorld.vue
@@ -24,7 +24,7 @@ export default {
         }
         else{
             alert('处理出现错误');
-            location.href='http://daojia.jingrunjia.com.cn';
+            location.href='http://www.itchun.com';
         }
         
   },
@@ -36,7 +36,7 @@ export default {
             .then(res=>{
                 if(res.data.info.code){
                     let openid=res.data.info.openId;
-                    location.href='http://daojia.jingrunjia.com.cn/login?openId='+openid;
+                    location.href='http://www.itchun.com/login?openId='+openid+'&companyId='+this.company;
                 }
                 else{
                    alert(res.data.msg);
